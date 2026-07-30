8e31339e14ea9b92e4a39b6ee03e627a0c13c8e6
Nanhebhai <gurjarnannehelal123@gmail.com>
2026-07-30 22:02:20 +0530

    Patterns

--- Patterns/pro11.cpp
+++ Patterns/pro11.cpp
@@ -4,8 +4,19 @@ using namespace std;
 int main() {
     int n = 5;
 
+    // Upper Part
     for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n - i; j++)
+            cout << " ";
 
+        for(int j = 1; j <= 2 * i - 1; j++)
+            cout << "*";
+
+        cout << endl;
+    }
+
+    // Lower Part
+    for(int i = n - 1; i >= 1; i--) {
         for(int j = 1; j <= n - i; j++)
             cout << " ";
 

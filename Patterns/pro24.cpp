a0a13b800b288149bf9f922f75b87410fb4d7e1a
Nanhebhai <gurjarnannehelal123@gmail.com>
2026-08-01 20:53:06 +0530

    Patterns

---
+++ Patterns/pro24.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n=6;
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if((i+j)%2==0)
+                cout<<"* ";
+            else
+                cout<<"  ";
+        }
+        cout<<endl;
+    }
+}
